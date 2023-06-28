4e8dd87a780cb82e7af31b02f810b64ec05e9ce2
Little-F1sh <itsnihon1@gmail.com>
2023-06-28 09:03:16 +0600

    03done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// slider Section
 let slideIndex = 0;
 showSlides();
 
@@ -16,4 +17,19 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000);
+}
+/* Accordion*/ 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
